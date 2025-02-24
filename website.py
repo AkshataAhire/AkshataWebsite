4d35df0bba59de538d822ccfff230943de959f7b
Akshata <53058275+AkshataAhire@users.noreply.github.com>
2025-02-24 23:07:54 +0530

    Update website.py
    Made changes in experiences and titles

--- website.py
+++ website.py
@@ -8,7 +8,7 @@ from htbuilder.funcs import rgba, rgb
 st.set_page_config(layout="wide")
 
 tab1, tab11, tab2, tab22, tab3, tab33, tab4, tab44, tab5, tab55, tab6, tab66, tab7 = st.tabs(
-    ["Home", "      ", "About Me", "      ", "Portfolio", "      ", "Skills", "      ", "Experience & Education", "      ", "My CV",
+    ["Home", "      ", "About Me", "      ", "Portfolio", "      ", "Skills", "      ", "Education & Eexperience", "      ", "My CV",
      "      ", "Contact"])
 
 with tab1:
@@ -18,7 +18,7 @@ with tab1:
     with title_container:
         with col1:
             st.title("Akshata Ahire")
-            st.subheader("AWS Solutions Architect | Data Scientist | DevOps Engineer")
+            st.subheader("Data Scientist | AWS Certified Solutions Architect | DevOps Engineer")
             st.write(
                 "Certified Scrum Master | Puppet Fundamentals trained | ISTQB Certified Tester | Oracle certified Java Professional")
         with col2:
@@ -34,19 +34,19 @@ with tab2:
         with col1:
             st.title("About Me")
             st.write(
-                "Hi! I am Akshata, a Data Scientist and a DevOps Engineer. I hold a Master’s degree with Commendation in Data Science (University of Aberdeen, UK) and a Bachelor’s degree in Computer Science (University of Mumbai, India).")
+                "Hi! I am Akshata, a Data Scientist, AWS Certified Solutions Architect and a DevOps Engineer. I hold a Master’s degree with Commendation in Data Science (University of Aberdeen, UK) and a Bachelor’s degree in Computer Science (University of Mumbai, India).")
             st.write(
-                "I was selected to work as a Data Scientist in an exclusive team on a **_funded research project_** for the United Nations with the University of Aberdeen and I have achieved the following results as part of the analysis:")
+                "I led key projects for the United Nations in an exclusive team on a **_funded research project_** with the University of Aberdeen, driving data analysis to identify critical SDGs for 2030 goals and achieved the following results as part of the analysis:")
             st.write(
                 "* India achieved 25% improvement towards successful implementation of the United Nations Sustainable Development Goals (SDGs) because of the Indian Government’s flagship programmes.")
             st.write(
                 "* The statistical analysis suggested that the indicator from SDG ‘Good Health and Well-being’ could prove to be a significant factor in the implementation of United Nations SDGs in India by 2030.")
             st.write(
-                "Additionally, on the basis of my Master's final research-project results, I was hired as a Data Scientist to provide insight on an internal project at the University of Aberdeen and I have achieved the below result using NLP:")
+                "Based on the results of my Master's final research project, I was hired as a Data Scientist to provide insight on a project at the University of Aberdeen. I leveraged Natural Language Processing (NLP) to achieve the following results:")
             st.write(
                 "* The qualitative analysis indicated that the awareness of the graduate attributes program should be raised with further improvements.")
             st.write(
-                "Furthermore, I have close to 2 years of experience working as a DevOps Engineer within the Insurance industry in the UK and close to 5 years of experience working in Operations and Software development in India.")
+                "I have 6+ years of Industry experience working on cloud platforms, CI/CD pipelines, data analysis and operations.")
             st.write(
                 "Overall I enjoy solving puzzles and to me, big data sets are one big puzzle I want to solve and the experiences gave me the ability to translate complex questions into understandable insights.")
 
@@ -168,7 +168,7 @@ with tab4:
             st.write("* Machine Learning | Data Modelling | K-Means | K-Nearest Neighbour | Linear Regression | Logistic Regression | Decision Tree")
             st.write("* Statistics | T-test | Hypothesis testing | Time series modelling")
             st.write("* Audio, Video & Image Analysis")
-            st.write("* DevOps | Microsoft Azure | AWS")
+            st.write("* DevOps | AWS | Microsoft Azure")
             st.write("* CI/CD | Puppet | Terraform | Atlassian Bamboo")
             st.write("* Python | R | Mathematica | Java")
             st.write("* NLTK | Spacy | Textacy")
@@ -184,61 +184,49 @@ with tab4:
             st.caption("(Representational Image) Photo: iStock")
 
 with tab5:
-    st.header("_Academic Experience_")
-    st.caption(
-        "As part of the Master's degree, every student has to work on a final project; However, I was selected to be part of an exclusive team on a **_funded research project_** for the United Nations as my Master's final year project.")
-    st.subheader("The Complex Interactions of the United Nations Sustainable Development Goals")
-    st.caption("Jan 2022 - April 2022 | Aberdeen, UK")
-    st.write(
-        "* Achieved significant results for the country India by investigating and contributing to the ongoing research activity of the TOSSIB project as a part of an exclusive team to find the complex interactions of the United Nations Sustainable Development Goals.")
-    st.write(
-        "* The Project was **_funded_** by the Royal Society to analyse the development of the United Nations 17 Sustainable Development Goals.")
-    st.write(
-        "* Performed quantitative data analysis on the indicators of the United Nations SDGs for country India using Python, Pandas and NumPy and graphically represented the data using python libraries like Matplotlib, NetworkX, Seaborn, Pyvis and Streamlit.")
-
-    st.write("")
-    st.header("_Professional Experience_")
-    st.subheader("University of Aberdeen | Data Scientist")
-    st.caption("April 2022 - May 2022 | Aberdeen, UK")
-    st.write("* After presenting my Master's final research project & results, I was hired to provide insight into an internal project of the University of Aberdeen.")
-    st.write("* UoA conducted a survey to understand the significance of the university's graduate attributes (the high level qualities and skills a student should gain as a result of the learning and experiences they engage with, while at university) and I was offered the role to analyse the survey results and present the same to the university.")
-    st.write("* Performed sentiment analysis on qualitative data of the graduate attributes survey conducted by the University, to understand the usefulness and further improve the Graduate Attributes.")
-    st.write(
-        "* Implemented statistical analysis using NLP with Spacy and Textacy to extract the keywords from the survey comments by measuring TF-IDF score and representing them in the Word Cloud.")
-
-    st.subheader("Expleo Technology UK Ltd | Technical - DevOps Engineer")
-    st.caption("July 2017 - April 2019 | London, UK")
-    st.write("* Successfully deployed 20+ releases as part of the Agile team at Hiscox as a Consultant.")
-    st.write(
-        "* Built and maintained the CI/CD pipeline in PaaS architecture and ensured stability and security of 15 environments including Staging and Production to ensure that the distributed team across the globe can bring beneficial advancements to the client’s customers and insurance experts.")
-    st.write("* Trained and mentored two juniors on DevOps tools and technologies.")
-    st.write("* Troubleshooting applications and environment issues with root cause analysis.")
-    st.write("* Expertise working with cloud platform MS Azure and various DevOps tools and technologies like Puppet, Terraform, Bamboo, Bitbucket, Git, GitHub, Artifactory, Splunk and a few more.")
-
-    st.subheader("Shree Sai Enterprises | Software Engineer")
-    st.caption("April 2016 - Sep 2016 | Mumbai, India")
-    st.write(
-        "* Developed and unit-tested modules for updating medicine inventory for in-house inventory and sales system in a pharmaceutical firm as part of an Agile team.")
-    st.write("* Developed interfaces in Java swing and wrote SQL queries to retrieve data.")
-    st.write(
-        "* Responsible for technical design, development and unit testing of modules for updating medicine inventory and purchase and sale receipts to local distributors.")
-
-    st.subheader("Marsh & McLennan Companies | Senior Consultant")
-    st.caption("Jan 2012 - Jan 2016 | Mumbai, India and London, UK")
-    st.write(
-        "* Successfully migrated the UK payroll system BenPal– automated payroll, employee benefits and pension benefits tool from the UK to India and worked for clients Royal London, Commerce Bank, Hitachi, The Perfume Shop, Vision Express and many more.")
-    st.write(
-        "* Provided ongoing support to the team in order to meet SLAs on Accuracy and productivity standards with internal/external stakeholders to ensure smooth and efficient deliveries.")
-    st.write("* Mentored new hires and supported the team in cross-skilling and upskilling initiatives.")
-    st.write("* Conducted regular refreshers to bridge the knowledge gap and regularly updated the process documentation.")
-    st.write("* Rewarded STAR OF THE MONTH and EB Archer Award for excellent work performance.")
-
     st.write("")
     st.header("_Education_")
     st.subheader("University of Aberdeen | M.Sc. Data Science with Commendation")
     st.caption("Jan 2021 - July 2022 | Aberdeen, UK")
     st.subheader("University of Mumbai | B.Sc. Computer Science")
     st.caption("Aug 2008 - July 2011 | Mumbai, India")
+    
+    st.header("_Experience_")
+    st.subheader("Freelance | Data Scientist")
+    st.caption("Jan 2023 - Present | Remote")
+    st.write("* Expert in data analysis of large qualitative and quantitative datasets using Python delivering actionable insights that improved decision-making by 25%.")
+    st.write("* Developed interactive dashboards with Python and other data visualisation tools, consistently thriving in agile, collaborative environments, contributing to success by learning from and mentoring others.")
+    st.write("* Led 10+ data science projects to successful, on-time delivery while maintaining high-quality standards. Mentored 15+ students in data synthesis, storytelling, and creation of monitoring reports to track project progress and performance.")
+
+    st.subheader("University of Aberdeen | Data Scientist")
+    st.caption("April 2022 - May 2022 | Aberdeen, UK")
+    st.write("* Spearheaded the development of the Graduate Attributes program that leveraged Natural Language Processing (NLP) in an Agile framework for triaging student feedback and further improve the Graduate Attributes, making the process 80% faster than the previous manual method.")
+    st.write("* Analysed qualitative survey results of the Graduate Attributes program using sentiment analysis and obtained 56% positive responses, indicating increased awareness of the program's significance.")
+    st.write("* Led the strategy and secured £200k funding for the university's flagship employability program, enhancing student soft skills. This initiative resulted in a 22% increase in job offers for participating students.")
+
+    
+    st.subheader("United Nations Project | Data Scientist")
+    st.caption("Jan 2022 - April 2022 | Aberdeen, UK")
+    st.write("* Selected as a Data Scientist for the prestigious TOSSIB project, funded by the Royal Society, to explore interactions among the 17 UN Sustainable Development Goals (SDGs) for India.")
+    st.write("* Analysed over 10,000 unstructured SDG indicator datasets and created 20+ visualisations using Python.")
+    st.write("* Collaborated with stakeholders to deliver actionable insights to the United Nations, identifying key SDGs to address India's most pressing challenges by 2030 through advanced Python clustering technique.")
+    
+    
+    st.subheader("Expleo Technology UK Ltd | Technical - DevOps Engineer")
+    st.caption("July 2017 - April 2019 | London, UK")
+    st.write("* Implemented CI/CD pipeline and automated infrastructure management for 10+ cloud environments using Terraform and Puppet, achieving 99% uptime and reducing manual efforts by 90% as part of an Agile project at Hiscox.")
+    st.write("* Collaborated with cross-functional teams to automate DevOps and data workflows, resulting in a 20% reduction in operational costs and improved process efficiency by enhancing system performance through effective monitoring and troubleshooting.")
+    st.write("* Led and mentored engineers on DevOps, ensuring a seamless 100% continuous workflow through collaboration with developers, testers, and stakeholders.")
+    st.write("* Expertise working with cloud platform MS Azure and various DevOps tools and technologies like Puppet, Terraform, Bamboo, Bitbucket, Git, GitHub, Artifactory, Splunk and a few more.")
+
+
+    st.subheader("Marsh & McLennan Companies | Senior Consultant")
+    st.caption("Jan 2012 - Jan 2016 | Mumbai, India and London, UK")
+    st.write("* Migrated 100% of the UK payroll system BenPal– automated payroll, employee benefits and pension benefits tool from the UK to India and worked for clients Royal London, Commerce Bank, Hitachi, The Perfume Shop, Vision Express and many more.")
+    st.write("* Led cross-functional communication with internal/external stakeholders, fostering cohesive collaboration between departments leading to a 20% increase in operational efficiency.")
+    st.write("* Mentored new hires and supported the team in cross-skilling and upskilling initiatives.")
+    st.write("* Rewarded STAR OF THE MONTH and EB Archer Award for excellent work performance.")
+
 
 with tab6:
     with open("AkshataAresume.png","rb") as file:
@@ -258,11 +246,11 @@ with tab7:
             st.write("")
             st.write("")
             st.markdown(
-                '<a href="https://www.linkedin.com/in/akshata-a-020a3b11b/"><p style="font-family:sans serif; color:Blue; font-size: 20px;">LinkedIn</p></a>',
+                '<a href="https://www.linkedin.com/in/akshataa/"><p style="font-family:sans serif; color:Blue; font-size: 20px;">LinkedIn</p></a>',
                 unsafe_allow_html=True)
             st.write("")
             st.markdown(
-                '<a href="mailto:akshata.aahire@gmail.com"><p style="font-family:sans serif; color:Blue; font-size: 20px;">Email me</p></a>',
+                '<a href="mailto:akshataahire01@gmail.com"><p style="font-family:sans serif; color:Blue; font-size: 20px;">Email me</p></a>',
                 unsafe_allow_html=True)
             st.write("")
             st.markdown(
